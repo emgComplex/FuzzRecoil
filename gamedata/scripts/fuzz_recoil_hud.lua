8094320c32eaa9daf254e405e15bc8456df2b8eb
Fuzz <fuzz@null.pointer>
2026-07-11 22:24:56 +0800

    refactor:hud recoil

---
+++ gamedata/scripts/fuzz_recoil_hud.lua
@@ -0,0 +1,7 @@
+local m_settings = settings or fuzz_recoil.settings
+local m_cfg = config or fuzz_recoil.config
+local utils = fuzz_recoil_utils
+local logger = fuzz_recoil_logger
+
+local M = {}
+_G.fuzz_recoil_hud_recoil = M
